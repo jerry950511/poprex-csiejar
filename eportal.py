1adfb61b49c7d72e8482a7fa9df7b29b9593eff6
lawrencelee0113 <lawrencelee0113@gmai.com>
2022-04-11 06:55:27 +0800

    upload (未完成)

--- eportal.py
+++ eportal.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-def check_eportal(account,password):
+def check_eportal(account,password,basic_data="no"):
 
     response = requests.get("https://wane.nutc.edu.tw/dm_device/device1.php?eportal_id="+account+"&eportal_passwd="+password+"&out=mem_check")
 
@@ -11,8 +11,8 @@ def check_eportal(account,password):
     # 把字串讀取成json
     soup = json.loads(text)
     if soup["res_echo"] == "pass":
-        return True
+        return {"res":True,"class":soup["class_no"],"name":soup["st_name"],"id":soup["st_id"],"phone":soup["phone"]}
     else:
-        return False
+        return {"res":False}
 
-print(check_eportal("s1111032033","Abcd0113"))
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, json, jsonify
 import eportal
+import uuid
 app = Flask(__name__)
 
 
@@ -8,6 +9,86 @@ app = Flask(__name__)
 def index_page():
   return render_template("index.html")
 
+def login_data(ac,eportaldata):
+  with open("static/data/amount.json") as file:
+    data = json.load(file)
+  if ac in data["accounts"]:
+    data["accounts"][ac]["login_time"]+=1
+  else:
+    data["accounts"][ac] = {
+      "basic":{
+        "id":eportaldata["id"],
+        "clicks":0,
+        "name":eportaldata["name"],
+        "class":eportaldata["class"]
+      },
+      "login_time":1,
+      "passcode":""
+    }
+  passcode = uuid.uuid4()
+  data["accounts"][ac]["passcode"] = passcode
+  with open("static/data/amount.json", "w") as file:
+    json.dump(data, file)
+  return passcode
+    
+
+@app.route('/eportal',methods=["POST"])
+def checkacpw():
+  ac = request.form.get("account")
+  pw = request.form.get("password")
+  eportaldata = eportal.check_eportal(ac,pw)
+  if eportaldata["res"]:
+    
+    return jsonify({"message":"true","passcode":login_data(ac,eportaldata)})
+  else:  
+    return jsonify({"message":"false"})
+
+def check_passcode(ac,passcode):
+  if ac == "":
+    return False
+  
+  with open("static/data/amount.json") as file:
+    data = json.load(file)
+  if data["accounts"][ac]["passcode"] == passcode:
+    return True
+  else:
+    return False
+
+def getnowdata():
+  with open("static/data/amount.json") as file:
+    data = json.load(file)
+  output = {"accounts":[]}
+  for i in data["accounts"]:
+    output["accounts"].append(data["accounts"][i]["basic"])
+  return output
+def uploadClick(ac,clicks):
+  with open("static/data/amount.json") as file:
+    data = json.load(file)
+  data["accounts"][ac]["basic"]["clicks"] += int(clicks)
+  with open("static/data/amount.json","w") as file:
+    json.dump(data, file)
+
+
+@app.route('/upload',methods=['POST'])
+def upload():
+  ac = request.form.get("account")
+  if ac == "":#no login
+    nowdata = getnowdata()
+    nowdata["message"] = "success"
+    return jsonify(nowdata)
+  else:#login
+    passcode = request.form.get("passcode")
+    if check_passcode(ac,passcode):#login success
+      clicks = request.form.get("clicks")
+      uploadClick(ac,clicks)
+      nowdata = getnowdata()
+      nowdata["message"] = "success"
+      return jsonify(nowdata)
+    else:#login fail
+      nowdata = getnowdata()
+      nowdata["message"] = "fail"
+      return jsonify(nowdata)
+    
 
 #run server
 if __name__ == "__main__":
